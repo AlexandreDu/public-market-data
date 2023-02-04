6b8ce3f97eb80a3b4caeccb2a74f6f771c7018d6
Alex <alexandre.dutay@outlook.fr>
2023-02-04 11:12:38 +0100

    fixing timestampToStr

--- src/utils/timestampToStr/index.ts
+++ src/utils/timestampToStr/index.ts
@@ -3,5 +3,5 @@ import { fromUnixTime, format } from 'date-fns';
 export function timestampToStr(timestamp: number): string {
   // fromUnixTime expect a timestamp in seconds and binance api returns a timestamp in ms
   const date = fromUnixTime(timestamp * 0.001);
-  return format(date, 'MM/dd/yyyy');
+  return format(date, 'dd/MM/yyyy');
 }
